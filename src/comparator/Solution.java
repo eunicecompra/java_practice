37c919934e4749fcf10a6ab6f61601341f7beb22
eunicecompra <eunicecompra@gmail.com>
2020-01-14 21:32:43 +1100

    Implemented compareTo solution

--- src/comparator/Solution.java
+++ src/comparator/Solution.java
@@ -3,6 +3,13 @@ import java.util.*;
 class Checker implements Comparator{
     @Override
     public int compare(Object o1, Object o2) {
+        Player p1 = (Player) o1;
+        Player p2 = (Player) o2;
+        if(p1.score < p2.score){
+            return -1;
+        }else if(p1.score == p2.score){
+            return p1.name.compareTo(p2.name);
+        }
         return 0;
     }
 }
