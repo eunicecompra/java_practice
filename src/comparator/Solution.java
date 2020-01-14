6978b8c17bff7423d4bdc13802d21287d8a59387
eunicecompra <eunicecompra@gmail.com>
2020-01-14 21:48:04 +1100

    Changed comparison order to satisfy challenge

--- src/comparator/Solution.java
+++ src/comparator/Solution.java
@@ -3,10 +3,10 @@ import java.util.*;
 class Checker implements Comparator<Player>{
     @Override
     public int compare(Player p1, Player p2) {
-        if(p1.score < p2.score){
+        if(p2.score < p1.score){
             return -1;
         }else if(p1.score == p2.score){
-            return p1.name.compareTo(p2.name);
+            return p2.name.compareTo(p1.name);
         }
         return 0;
     }
